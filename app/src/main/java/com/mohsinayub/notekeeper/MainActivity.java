844571fdb225b05fc5a00b37f490580a0582ef3f
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-07-26 10:32:02 +0200

    test small behavior to make sure environment is working

--- app/src/main/java/com/mohsinayub/notekeeper/MainActivity.java
+++ app/src/main/java/com/mohsinayub/notekeeper/MainActivity.java
@@ -1,12 +1,12 @@
 package com.mohsinayub.notekeeper;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            TextView textView = findViewById(R.id.text_value);
+            String stringValue = textView.getText().toString();
+            int originalValue = Integer.parseInt(stringValue);
+            int newValue = MyWorker.doubleTheValue(originalValue);
+            stringValue = Integer.toString(newValue);
+            textView.setText(stringValue);
+
+            final String message = "Changed value " + originalValue + " to " + newValue;
+            Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null)
+                    .show();
+        });
+    }
+}
+
+class MyWorker {
+    public static int doubleTheValue(int value) {
+        return value * 2;
     }
 }
