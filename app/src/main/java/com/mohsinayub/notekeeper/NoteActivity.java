45118b874ae484b74a5e1eeb969b96ac92e59b0c
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-07-27 03:11:27 +0200

    remove code associated with fab

--- app/src/main/java/com/mohsinayub/notekeeper/NoteActivity.java
+++ app/src/main/java/com/mohsinayub/notekeeper/NoteActivity.java
@@ -3,12 +3,10 @@ package com.mohsinayub.notekeeper;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 public class NoteActivity extends AppCompatActivity {
 
