3e223ccb4dfbf2421b0f1edb2c43b286e3db0e39
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-07-27 21:13:24 +0200

    add note list activity

---
+++ app/src/main/java/com/mohsinayub/notekeeper/NoteListActivity.java
@@ -0,0 +1,29 @@
+package com.mohsinayub.notekeeper;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.widget.Toolbar;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAnchorView(R.id.fab)
+                .setAction("Action", null).show();
+        });
+    }
+}
